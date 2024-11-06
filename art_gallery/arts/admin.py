a9ffa1e949779cc90f0462ec855b6bc2010bdde8
ahmed200346 <AhmedAziz.Ammar@esprit.tn>
2024-11-06 07:05:23 +0100

    Upload gestionArtistes Admin.py+Models.py (User+Art+Artistes)

--- art_gallery/arts/admin.py
+++ art_gallery/arts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from artistes.models import Artistes
+from .models import Art
 
-# Register your models here.
+@admin.register(Art)
+class ArtAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner')
+    list_filter = ('owner',)
+    search_fields = ('title', 'tags', 'owner__username')  
+    readonly_fields = ('created_at',)  
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'file', 'tags', 'owner')
+        }),
+        ('Date Information', {
+            'fields': ('created_at', ),
+            'classes': ('collapse',),
+        }),
+    )
--- art_gallery/arts/models.py
+++ art_gallery/arts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
+from django.core.exceptions import ValidationError
+from artistes.models import Artistes
 
-# Create your models here.
+class Art(models.Model):
+    id_art = models.AutoField(primary_key=True)
+    reference = models.CharField(max_length=50, unique=True)
+    owner = models.ForeignKey(Artistes, on_delete=models.CASCADE, related_name='arts') 
+    title = models.CharField(max_length=100,unique=True)
+    description = models.TextField(blank=True, null=True)
+    category = models.CharField(max_length=50, blank=True, null=True) 
+    tags = models.CharField(max_length=255, blank=True, help_text="Tags séparés par des virgules")
+    created_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(
+        upload_to='arts/',
+        validators=[
+            FileExtensionValidator(allowed_extensions=['jpg', 'png', 'mp4', 'mp3'],message="Arts doit etre soit musique .mp3 ou .mp4 soit image :. jpg ou .png soit vidéo .mp4")
+        ]
+    )
+    def __str__(self):
+        return f"{self.title} "
+    @property
+    def tags_list(self):
+        return [tag.strip() for tag in self.tags.split(',') if tag.strip()]
